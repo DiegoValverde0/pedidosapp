310cac24c906f44af8b639cd0f38fa24f6d6d87d
DiegoValverde0 <166634144+DiegoValverde0@users.noreply.github.com>
2024-08-26 19:17:50 -0400

    Add files via upload

---
+++ DesarrolloDeSistema/CapaDatos/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public abstract class DbConnection
+    {
+        public static string cn = "Server=VALVERDE-PC\\SQLEXPRESS;Database=DbventasDesarrollo;User ID=sa;Password=Aa1234567*;Trusted_Connection=true;Encrypt=false;MultipleActiveResultSets=true";
+
+
+        private readonly string connectionString;
+        public DbConnection()
+        {
+            connectionString = cn;
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ DesarrolloDeSistema/PedidosApp/FrmCategoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PedidosApp
+{
+    public partial class FrmCategoria : Form
+    {
+        public FrmCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
